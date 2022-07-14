1af49a3724c21b08c116d9999f8df5d0f482ddc5
Levi <thegood878@gmail.com>
2022-07-14 22:44:07 +0000

    polindo primeiro-script.sh

--- primeiro-script.sh
+++ primeiro-script.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#########################################
+#
+# Name: primeiro-script.sh - Script teste do curso da Udemy
+# Author: Levi de Almeida Pontes
+# Email para contato: thegood878@gmail.com
+#
+#########################################
+
+# Variaveis usadas no script
+HORA="$(date +%H:%M)"
+DIA="$(date +%d)"
+MES="$(date +%m)"
+LISTCOLEGAS="/home/ubuntu/cursoscripts/arquivos-importantes/listacolegas.txt"
+
 echo "==========Script 01=========="
 echo -e "\n"
-echo -e "\n"
-echo -e "\n"
-git status
-echo "Script executado em:"
-date
+echo "Script executado as: $HORA do dia $DIA do mes $MES"
+echo "==========================================="
+echo "Lista de colegas de trabalho"
+sort $LISTCOLEGAS
