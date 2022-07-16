8845e9fe6a01762b596aadb3ef62a1b8878679b9
Levi <thegood878@gmail.com>
2022-07-16 22:55:47 +0000

    Adicionando mensagem de erro a calculadora.sh

--- calculadora.sh
+++ calculadora.sh
@@ -4,4 +4,4 @@ read -p "Insira o primeiro valor: " VALOR1
 read -p "Insira o segundo valor: " VALOR2
 echo "Insira a operação [soma (+) subtração(-) multiplicação(*) divisão(/):"
 read OPERACAO
-echo "Resultado: $(expr $VALOR1 "$OPERACAO" $VALOR2)"
+echo "Resultado: $(expr $VALOR1 "$OPERACAO" $VALOR2 2> /dev/null || echo "Alguma informação entregue está errada, por favor verifique e tente novamente")"
