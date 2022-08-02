6b0c61bd57be9a86847153865e7224188f271fa2
Levi <thegood878@gmail.com>
2022-08-02 21:51:38 +0000

    relatorio-usuario.sh finalizado

--- relatorio-usuario.sh
+++ relatorio-usuario.sh
@@ -14,3 +14,20 @@
 ##################################################################################
 
 ls /home/$1 > /dev/null 2>&1 || { echo "Usuario inexistente" ; exit 1 ; }
+
+UIDUSUARIO=$( cat /etc/passwd | grep $1 | cut -d : -f3 )
+DESCRICAO=$( cat /etc/passwd | grep $1 | cut -d : -f5 )
+USOHOME=$( du -sh /home/$1 | cut -f1 ) 
+
+
+clear
+echo "==============================================================================="
+echo "Relatorio do usuario: $1"
+echo ""
+echo "UID do usuario: $UIDUSUARIO"
+echo "Descrição do usuario: $DESCRICAO"
+echo "Total usado no /home/$1: $USOHOME"
+echo ""
+lastlog -u $1
+echo "==============================================================================="
+exit 0
